120b06aa3478707d60eff258897535189bdc23e8
Cyrruz John <cyrruz.varquez05@gmail.com>
2023-03-21 23:24:45 +0800

    upload to git

--- app/Views/admin_page.php
+++ app/Views/admin_page.php
@@ -15,7 +15,7 @@
   <body>
     <div class="container">
         <h1 class="text-center ">Welcome <?php echo $user['username'] ?>!</h1>
-        <a href="http://localhost/codeigniter/public/logout">testtest</a>
+        <a href="http://localhost/codeigniter/public/logout">Logout</a>
   
 </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
