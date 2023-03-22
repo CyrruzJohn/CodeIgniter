079abef59958cd7e42c99851aafb2755dc1edede
Cyrruz John <cyrruz.varquez05@gmail.com>
2023-03-22 12:44:51 +0800

    commit to git

--- app/Views/login_page.php
+++ app/Views/login_page.php
@@ -13,7 +13,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
   </head>
   <body>
-    <div class="container">
+  <div class="container py-5" style="background-color:white; max-width: 400px; border-radius: 25px; box-shadow: 0 0 15px rgba(0, 0, 0, 0.3); height: 600px; border: none; outline: none; ">
         <h2 class="text-center mt-5">Login to URL Shortening System</h2>
         <form method="post" action="http://localhost/codeigniter/public/process/login" class="mx-auto mt-5" style="max-width: 320px">
             <div class="mb-3">
@@ -25,15 +25,32 @@
                 <input class="form-control" type="password" name="pass" />
             </div>
             <div class="mb-3 d-flex justify-content-center align-items-center">
-                <button class="btn btn-primary" type="submit">Login</button>
+            <button class="btn btn-primary" type="submit">Login</button>
             </div>
             <div class="mb-3 d-flex justify-content-center align-items-center">
-              Don't have an account yet?  <a href="http://localhost/codeigniter/public/register"> sign up</a>
-
+            Don't have an account yet? <a href="http://localhost/codeigniter/public/register">Sign up</a>
             </div>
         </form>
+  
+</div>
   
 </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
   </body>
 </html>
+<style>
+  html, body {
+  height: 100%;
+  width: 100%;
+  margin: 0;
+}
+body {
+  background: radial-gradient(circle at 10% 20%, rgb(0, 93, 133) 0%, rgb(0, 181, 149) 90%);
+}
+.container {
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+}
+</style>
--- app/Views/register_page.php
+++ app/Views/register_page.php
@@ -9,13 +9,11 @@
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Register</title>
-    <link rel="stylesheet" href="stylesheet1.css">
-
+    <title>Bootstrap demo</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
   </head>
   <body>
-  <div class="container py-5 h-50 mt-5 " style="border: 1px solid black; background-color:white; max-width: 400px; border-radius: 25px;   box-shadow: 0 0 10px rgba(0, 0, 0, 0.3);">
+  <div class="container py-5" style="background-color:white; max-width: 400px; border-radius: 25px; box-shadow: 0 0 15px rgba(0, 0, 0, 0.3); height: 600px; border: none; outline: none; ">
         <h2 class="text-center mt-5">Register</h2>
         <form method="post" action="http://localhost/codeigniter/public/process/login" class="mx-auto mt-5" style="max-width: 320px">
             <div class="mb-3">
@@ -31,18 +29,30 @@
                 <input class="form-control" type="password" name="pass" />
             </div>
             <div class="mb-3 d-flex justify-content-center align-items-center">
-            <button class="btn btn-primary" type="submit">Login</button>
+            <button class="btn btn-primary" type="submit">Submit</button>
             </div>
+            
         </form>
+  
+</div>
   
 </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
   </body>
-  <style>
-body {background-color: #6096B4;
-    }
-
-h1   {color: blue;}
-p    {color: red;}
-</style>
 </html>
+<style>
+  html, body {
+  height: 100%;
+  width: 100%;
+  margin: 0;
+}
+body {
+  background: radial-gradient(circle at 10% 20%, rgb(0, 93, 133) 0%, rgb(0, 181, 149) 90%);
+}
+.container {
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+}
+</style>
